20fdfd618626416ca2c78b6319b78c5b4b671ad8
EthanBar <ethanbarker0@gmail.com>
2022-02-26 17:30:12 -0800

    cube rootin

--- 1.1/1.07.rkt
+++ 1.1/1.07.rkt
@@ -2,12 +2,6 @@
 
 (define (square x) (* x x))
 
-(define (new-if predicate
-                then-clause
-                else-clause)
-  (cond (predicate then-clause)
-        (else else-clause)))
-
 (define (average x y)
   (/ (+ x y) 2))
 
